6cc1fa615a794a7e16ed2250811d2cc16769be7b
Michael Scholten <michael-scholten@hotmail.nl>
2025-04-11 12:54:59 +0200

    increased reflectance and added gamma correction to make the brightness changes more consistent

--- include/Camera.hpp
+++ include/Camera.hpp
@@ -61,7 +61,7 @@ public:
         HitRecord record;
         if(world.hit(ray, Interval(0.001, INFINITY), record)){
             const Vec3 direction = record.normal.random_on_hemisphere();
-            return 0.5 * ray_color(Ray(record.point, direction), depth_left - 1, world);
+            return 0.7 * ray_color(Ray(record.point, direction), depth_left - 1, world);
         }
 
         const Vec3 unit_direction = ray.direction().unit_vector();
--- include/Color.hpp
+++ include/Color.hpp
@@ -5,11 +5,15 @@
 
 using Color = Vec3;
 
+inline double linear_to_gamma(const double linear_component){
+    return linear_component > 0? std::sqrt(linear_component) : 0;
+}
+
 inline std::ostream& write_color(std::ostream& out, const Color& pixel_color){
     static const Interval intensity(0, 0.999);
-    const uint16_t red = static_cast<uint8_t>(256 *  intensity.clamp(pixel_color.x()));
-    const uint16_t green = static_cast<uint8_t>(256 * intensity.clamp(pixel_color.y()));
-    const uint16_t blue = static_cast<uint8_t>(256 * intensity.clamp(pixel_color.z()));
+    const uint16_t red = static_cast<uint8_t>(256 *  intensity.clamp(linear_to_gamma(pixel_color.x())));
+    const uint16_t green = static_cast<uint8_t>(256 * intensity.clamp(linear_to_gamma(pixel_color.y())));
+    const uint16_t blue = static_cast<uint8_t>(256 * intensity.clamp(linear_to_gamma(pixel_color.z())));
 
     out << red << ' ' << green << ' ' << blue << '\n';
     return out;
